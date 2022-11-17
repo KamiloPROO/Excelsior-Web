8621f3762e5b5ef290a87e13f83f463b81f8554b
AndresWnj <andres.wnj@gmail.com>
2022-11-17 10:58:19 -0500

    listo funcion click

--- Dashboard/assets/js/logica/utils/HttpsClient.js
+++ Dashboard/assets/js/logica/utils/HttpsClient.js
@@ -10,20 +10,20 @@ function CallEndPointPost(url, jsonArgs) {
 
                 const idper = data.Id;
 
-              if (data.CodigoRespuesta === 200 ){
+              // if (data.CodigoRespuesta === 200 ){
 
-                localStorage.setItem("idPersona",idper);
-                window.location.href = URL_API_BASE;
-                location.replace("Dash.html");
-                location.href ="Dash.html";
+              //   localStorage.setItem("idPersona",idper);
+              //   window.location.href = URL_API_BASE;
+              //   location.replace("Dash.html");
+              //   location.href ="Dash.html";
 
-              }else{
-                Swal.fire({
-                  icon: 'error',
-                  title: 'Oops...',
-                  text: 'Usuario o Contraseña Incorrecta',
-                })
-              };
+              // }else{
+              //   Swal.fire({
+              //     icon: 'error',
+              //     title: 'Oops...',
+              //     text: 'Usuario o Contraseña Incorrecta',
+              //   })
+              // };
         },
         error: function () {
             alert("Ha ocurrido un error, por favor vuelva a intentarlo")            
