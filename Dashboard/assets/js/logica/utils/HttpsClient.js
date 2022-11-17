8294117ba9489e72a1d8dc80587b610dd1399e41
AndresWnj <andres.wnj@gmail.com>
2022-11-17 15:59:16 -0500

    prueba num 2

--- Dashboard/assets/js/logica/utils/HttpsClient.js
+++ Dashboard/assets/js/logica/utils/HttpsClient.js
@@ -8,26 +8,26 @@ function CallEndPointPost(url, jsonArgs) {
                 console.log(data.Id);
                 console.log(data);
 
-                // const idper = data.Id;
+                const idper = data.Id;
                 const name = data.PrimerNombre;
 
-                console.log("holaaaaaaa", name);
+                console.log(name);
+                
+              if (data.CodigoRespuesta === 200 ){
 
-              // if (data.CodigoRespuesta === 200 ){
+                localStorage.setItem("idPersona",idper);
+                localStorage.setItem("PrimerNombre",name);
+                window.location.href = URL_API_BASE;
+                location.replace("Dash.html");
+                location.href ="Dash.html";
 
-              //   localStorage.setItem("idPersona",idper);
-              //   localStorage.setItem("PrimerNombre",name);
-              //   window.location.href = URL_API_BASE;
-              //   location.replace("Dash.html");
-              //   location.href ="Dash.html";
-
-              // }else{
-              //   Swal.fire({
-              //     icon: 'error',
-              //     title: 'Oops...',
-              //     text: 'Usuario o Contraseña Incorrecta',
-              //   })
-              // };
+              }else{
+                Swal.fire({
+                  icon: 'error',
+                  title: 'Oops...',
+                  text: 'Usuario o Contraseña Incorrecta',
+                })
+              };
         },
         error: function () {
             alert("Ha ocurrido un error, por favor vuelva a intentarlo")            
