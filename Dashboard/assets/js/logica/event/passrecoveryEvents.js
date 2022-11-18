89c0a1bfb54534ee4d8d28765afdb3732bcc550a
AndresWnj <andres.wnj@gmail.com>
2022-11-18 18:57:26 -0500

    algo se hizo

--- Dashboard/assets/js/logica/event/passrecoveryEvents.js
+++ Dashboard/assets/js/logica/event/passrecoveryEvents.js
@@ -3,6 +3,32 @@ const recovey = document.getElementById("recoveryContrasenia")
 
 recovey.addEventListener("click", function(e){
 
-    passRecovery()
+    const userr = document.getElementById("recoveryUserio").value
+    const correoo = document.getElementById("recoveryEmailContra").value
+
+    if ( userr === null || userr === "" ){
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Ingresa Tu Usuario',
+            
+          })
+
+      } else if ( correoo ===  null || correoo === "" ){
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Ingresa Tu Correo',
+          })
+
+      }else{
+
+        passRecovery(); 
+
+      }
+
+    
 
 })
