84dde3b652af6551c1c0940e477df69773edd19c
kamiloPROO <fotocopiasa100@gmail.com>
2023-02-01 08:57:53 -0500

    arreglos dashboard listar planes y qr

--- Dashboard/assets/js/logica/utils/HttpsClientLisInversion.js
+++ Dashboard/assets/js/logica/utils/HttpsClientLisInversion.js
@@ -1,60 +1,30 @@
-// function CallEndPointPostListInver(url, jsonArgs) {
-//     $.ajax({
-//         type: "POST",
-//         url: url,
-//         data: jsonArgs,
-//         success: function (data) {
-
-//           console.log(data);
-
-//           for (const datos of data){
-
-//            const productList = document.getElementById("product-list");
-//            const element = document.createElement("div")
-
-          
-//            element.innerHTML += `
-//            <div class="planess">
-//            <div class="cardInversion" id="plan2">
-              
-//                <img src="assets/images/BLUE.png" alt="Avatar" style="width:50%"> 
-//                <div class="">
-//                  <h3 ><b>PROGRESS OF MY PLAN</b></h3>
-//                  <br>
-//                  <h3>Rendimiento Mensual %</h3>
-//                  <h3>${datos.Rendimiento}</h3>
-//                  <br>
-//                  <h3>${datos.RangoInicial}</h3>
-//                  <h3>${datos.RangoFinal}</h3>
-//                  <br>
-//                  <h3>Amount Invested USD</h3>
-//                  <h3 style="color: rgb(7, 220, 23)">${datos.Cant_invertir}</h3>
-//                </div>
-           
-//            </div>
-//          </div>
-//         `;
-
-//         productList.appendChild(element)
-// }
-//         },
-//         error: function () {
-//             alert("Ha ocurrido un error, por favor vuelva a intentarlo")            
-//         }
-//     });
-// }
-
 
 document.addEventListener('DOMContentLoaded', e => {
 
   var idpersona = localStorage.getItem("idPersona");
 
+  if (idpersona === "" || idpersona === null) {
+
+    Swal.fire({
+      icon: 'error',
+      title: 'Opsss',
+      text: 'Primero inicia sesion',
+    }).then((result) => {
+
+      if (result.isConfirmed) {
+        window.location.href = URL_API_BASE;
+        location.replace("signIn.html");
+        location.href = "signIn.html";
+      }
+    })
+  }
+
   const nombre = document.getElementById("name")
   const balanceinvest = document.getElementById("balanceInvest")
   var namee = localStorage.getItem("PrimerNombre")
- 
+
   var resBalnace = localStorage.getItem("balInvest")
-      
+
   nombre.textContent = namee;
   balanceinvest.textContent = resBalnace;
 
@@ -63,7 +33,7 @@ document.addEventListener('DOMContentLoaded', e => {
   $.ajax({
     type: "POST",
     url: "https://excelsiortrader.app/api/Inversiones/ListarMisInversiones",
-    data: {"IdPersona":idpersona},
+    data: { "IdPersona": idpersona },
     success: function (data) {
 
       console.log(data);
@@ -72,15 +42,15 @@ document.addEventListener('DOMContentLoaded', e => {
 
       var metaData = data.Data;
 
-      for (const datos of metaData){
+      for (const datos of metaData) {
 
-       const productList = document.getElementById("product-list");
-       const element = document.createElement("div")
-       
-      
-       console.log(namee);
+        const productList = document.getElementById("product-list");
+        const element = document.createElement("div")
 
-       element.innerHTML += `
+
+        console.log(namee);
+
+        element.innerHTML += `
        <div class="planess">
        <div class="cardInversion" id="plan2">
           
@@ -102,13 +72,13 @@ document.addEventListener('DOMContentLoaded', e => {
      </div>
     `;
 
-    productList.appendChild(element)
-}
+        productList.appendChild(element)
+      }
     },
     error: function () {
-        alert("Ha ocurrido un error, por favor vuelva a intentarlo")            
+      alert("Ha ocurrido un error, por favor vuelva a intentarlo")
     }
-});
+  });
 
 
 })
